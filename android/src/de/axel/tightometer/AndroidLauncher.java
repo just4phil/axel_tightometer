dceb1d1b9e093469a29c06fa5dd329a84813f30b
andre schmode <andre.schmode@googlemail.com>
2020-04-25 21:16:32 +0200

    initial commit

---
+++ android/src/de/axel/tightometer/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package de.axel.tightometer;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import de.axel.tightometer.Tightometer;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Tightometer(), config);
+	}
+}
---
+++ desktop/src/de/axel/tightometer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.axel.tightometer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.axel.tightometer.Tightometer;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Tightometer(), config);
+	}
+}
